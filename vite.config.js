a465be7437092764a3ce5b9cb68e35a6a84fab17
Your Name <eze_adnah@yahoo.com>
2025-04-05 19:18:50 +0100

    Fixed vite config

--- vite.config.js
+++ vite.config.js
@@ -3,17 +3,11 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  publicDir: 'public',
   build: {
     outDir: 'dist',
     emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        main: './public/index.html'
-      }
-    }
   },
   resolve: {
-    extensions: ['.js', '.jsx', '.ts', '.tsx']
-  }
+    extensions: ['.js', '.jsx'],
+  },
 });
